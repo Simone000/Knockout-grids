4c2a5fd4bf87569d822412caa172e618bdf0bffc
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-02-03 17:45:40 +0100

    temp fake chiamata server

--- KnockoutGrids/KnockoutGrids/App/kogrids-serversample1/kogrids-serversample1.js
+++ KnockoutGrids/KnockoutGrids/App/kogrids-serversample1/kogrids-serversample1.js
@@ -36,25 +36,54 @@ function pageModel(params) {
 
     self.dipendenti = ko.observableArray();
 
-    self.loadDipendenti = function () {
-        var dipendenti_tmp = new Array();
-        dipendenti_tmp.push(new Dipendente({ ID: 1, Nome: "Dipendente 1", Reparto: { ID: 1, Nome: "Reparto 1", Azienda: { ID: 1, Nome: "Azienda 1" } } }));
-        dipendenti_tmp.push(new Dipendente({ ID: 2, Nome: "Dipendente 2", Reparto: { ID: 2, Nome: "Reparto 2", Azienda: { ID: 3, Nome: "Azienda 3" } } }));
-        dipendenti_tmp.push(new Dipendente({ ID: 3, Nome: "Dipendente 3", Reparto: { ID: 3, Nome: "Reparto 3", Azienda: { ID: 2, Nome: "Azienda 2" } } }));
-        dipendenti_tmp.push(new Dipendente({ ID: 4, Nome: "Dipendente 4", Reparto: { ID: 1, Nome: "Reparto 1", Azienda: { ID: 1, Nome: "Azienda 1" } } }));
-        dipendenti_tmp.push(new Dipendente({ ID: 5, Nome: "Dipendente 5", Reparto: { ID: 2, Nome: "Reparto 2", Azienda: { ID: 3, Nome: "Azienda 3" } } }));
-        dipendenti_tmp.push(new Dipendente({ ID: 6, Nome: "Dipendente 6", Reparto: { ID: 3, Nome: "Reparto 3", Azienda: { ID: 2, Nome: "Azienda 2" } } }));
+    self.loadDipendenti = function (PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search) {
+        //var dipendenti_tmp = new Array();
+        //dipendenti_tmp.push(new Dipendente({ ID: 1, Nome: "Dipendente 1", Reparto: { ID: 1, Nome: "Reparto 1", Azienda: { ID: 1, Nome: "Azienda 1" } } }));
+        //dipendenti_tmp.push(new Dipendente({ ID: 2, Nome: "Dipendente 2", Reparto: { ID: 2, Nome: "Reparto 2", Azienda: { ID: 3, Nome: "Azienda 3" } } }));
+        //dipendenti_tmp.push(new Dipendente({ ID: 3, Nome: "Dipendente 3", Reparto: { ID: 3, Nome: "Reparto 3", Azienda: { ID: 2, Nome: "Azienda 2" } } }));
+        //dipendenti_tmp.push(new Dipendente({ ID: 4, Nome: "Dipendente 4", Reparto: { ID: 1, Nome: "Reparto 1", Azienda: { ID: 1, Nome: "Azienda 1" } } }));
+        //dipendenti_tmp.push(new Dipendente({ ID: 5, Nome: "Dipendente 5", Reparto: { ID: 2, Nome: "Reparto 2", Azienda: { ID: 3, Nome: "Azienda 3" } } }));
+        //dipendenti_tmp.push(new Dipendente({ ID: 6, Nome: "Dipendente 6", Reparto: { ID: 3, Nome: "Reparto 3", Azienda: { ID: 2, Nome: "Azienda 2" } } }));
+        //self.dipendenti(dipendenti_tmp);
 
-        self.dipendenti(dipendenti_tmp);
+        $.ajax({
+            url: 'api/Aziende/GetAziendePaged/',
+            dataType: 'json',
+            asyc: true,
+            type: "get",
+            success: function (data) {
+                self.dipendenti(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("errore!");
+            }
+        });
     };
-    self.loadDipendenti();
+    self.loadDipendenti(5, 0, 'Nome', true, 'reparto.azienda.nome', '');
 
     self.dipendentiPaged = ko.observable(new knockoutgrids.ServerGrid(self.dipendenti(), 5, 'reparto.azienda.nome',
             function (PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search) { //OnChange
                 console.log("OnChange" + " PageSize:" + PageSize + " CurrPage:" + CurrPage + " SortBy:" + SortBy + " IsDesc:" + IsDesc + " SearchBy:" + SearchBy + " Search:" + Search);
-                self.loadDipendenti();
+                if (!Search)
+                    Search = '';
+                self.loadDipendenti(PageSize, CurrPage, SortBy, IsDesc, SearchBy, Search);
             })
     );
+
+    /*
+    self.loadAziende = function () {
+        function success(data) {
+            self.aziende(data);
+
+            self.id_azienda(params.id_azienda);
+        };
+        function error(jqXHR, desc) {
+            toastr["error"](desc, "Errore!");
+        };
+        api.GetAziendeList($('#div_listAziende'), success, error);
+    };
+    self.loadAziende();
+    */
 
     return self;
 };
--- KnockoutGrids/KnockoutGrids/Controllers/AziendeController.cs
+++ KnockoutGrids/KnockoutGrids/Controllers/AziendeController.cs
@@ -9,8 +9,8 @@ namespace KnockoutGrids.Controllers
 {
     public class AziendeController : ApiController
     {
-        [HttpGet]
-        public IHttpActionResult GetAziende()
+        [NonAction]
+        private List<AziendaModel> GetAziendeModels()
         {
             var aziende = new List<AziendaModel>();
             aziende.Add(new AziendaModel()
@@ -29,14 +29,92 @@ namespace KnockoutGrids.Controllers
                             {
                                 ID = 1,
                                 Nome = "Dipendente 1"
+                            },
+                            new DipendenteModel()
+                            {
+                                ID = 2,
+                                Nome = "Dipendente 2"
+                            },
+                            new DipendenteModel()
+                            {
+                                ID = 3,
+                                Nome = "Dipendente 3"
+                            }
+                        }
+                    },
+                    new RepartoModel()
+                    {
+                        ID = 2,
+                        Nome = "Reparto 2",
+                        Dipendenti = new List<DipendenteModel>()
+                        {
+                            new DipendenteModel()
+                            {
+                                ID = 4,
+                                Nome = "Dipendente 4"
+                            },
+                            new DipendenteModel()
+                            {
+                                ID = 5,
+                                Nome = "Dipendente 5"
+                            },
+                            new DipendenteModel()
+                            {
+                                ID = 6,
+                                Nome = "Dipendente 6"
                             }
                         }
                     }
                 }
             });
+            aziende.Add(new AziendaModel()
+            {
+                ID = 1,
+                Nome = "Azienda 1",
+                Reparti = new List<RepartoModel>()
+                {
+                    new RepartoModel()
+                    {
+                        ID = 1,
+                        Nome = "Reparto 1",
+                        Dipendenti = new List<DipendenteModel>()
+                        {
+                            new DipendenteModel()
+                            {
+                                ID = 1,
+                                Nome = "Dipendente 1"
+                            },
+                            new DipendenteModel()
+                            {
+                                ID = 2,
+                                Nome = "Dipendente 2"
+                            },
+                            new DipendenteModel()
+                            {
+                                ID = 3,
+                                Nome = "Dipendente 3"
+                            }
+                        }
+                    }
+                }
+            });
+            return aziende;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAziende()
+        {
+            return Ok(GetAziendeModels());
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAziendePaged(int PageSize, int CurrPage, string SortBy, bool IsDesc, string SearchBy, string Search)
+        {
+            var aziende = GetAziendeModels();
 
             return Ok(aziende);
         }
+
 
         public class AziendaModel
         {
