4816b7b9a300c09c84937737d3f5e32211cbee48
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-11-27 20:47:45 +0100

    filters ws

--- KnockoutGrids/KnockoutGrids/Scripts/_references.js
+++ KnockoutGrids/KnockoutGrids/Scripts/_references.js
@@ -3,6 +3,7 @@
 /// <reference path="../app/tests/kogrids-server/kogrids-server.js" />
 /// <reference path="../app-samples/kogrids-client/kogrids-client.js" />
 /// <reference path="../app-samples/kogrids-client-filters/kogrids-client-filters.js" />
+/// <reference path="../app-samples/kogrids-client-filters-ws/kogrids-client-filters-ws.js" />
 /// <reference path="../app-samples/kogrids-client-fixedcolumn/kogrids-client-fixedcolumn.js" />
 /// <reference path="../app-samples/kogrids-server/kogrids-server.js" />
 /// <reference path="bootstrap.min.js" />
